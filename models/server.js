2b070565cfee18b8d30dcaa0619aa1c8ae9de087
Lucas Segovia <lucas.segovia210@gmail.com>
2022-04-11 23:03:54 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors');
+
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+
+        //MIDDLEWARES
+        this.middlewares();
+
+        //RUTAS DE MI APP
+        this.routes();
+    }//FIN CONSTRUCTOR
+    
+    middlewares(){
+
+        //CORS
+        this.app.use(cors());
+
+        //LECTURA Y PARSEO DEL BODY
+        this.app.use(express.json());
+
+        //DIRECTORIO PUBLICO
+        this.app.use(express.static('public'));
+
+    }//FIN MIDD
+
+    routes(){
+        this.app.use(this.usuariosPath,require('../routes/usuarios'));
+
+
+    }//FIN ROUTES
+
+    listen(){
+        this.app.listen(this.port,()=>{
+        console.log('Servidor corriendo en puerto',this.port);
+        });
+    }
+
+}//FIN SERVER
+
+module.exports = Server;
